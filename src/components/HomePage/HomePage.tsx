1e3090316ef95c2f9fe0fdf759431583f836b224
Joebangles <qhcbest@gmail.com>
2026-02-17 01:24:53 -0800

    UI Changes

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,37 +3,39 @@ import "./HomePage.css";
 // import headshot from '../../assets/feb_headshot.jpg'
 // import headshot2 from '../../assets/DSC02704_crop3.jpg'
 import headshot_edited from '../../assets/DSC02704_crop4_edited.jpg'
+import github_logo from '../../assets/github_logo.svg'
+import linkedin_logo from '../../assets/linkedin_logo.png'
 
 function App() {
     return (
-        <>
-        <div className="frontpage-name">
-            Quinn Colello
-        </div>
         <div className="frontpage-content">
-            <div className="frontpage-left-column">
+            <div className="frontpage-profile">
                 <img src={headshot_edited} alt="profile picture" className="frontpage-headshot" />
                 <div className="profile-text">
-                    <span>Quinn Colello</span>
-                    <span className="profile-text-line">B.A. in Computer Science</span>
-                    <span className="profile-text-line">B.A. in Applied Mathematics</span>
+                    <span className="frontpage-name">Quinn Colello</span>
                     <span className="profile-text-line">Berkeley, CA - Carlsbad, CA</span>
-                    <span className="profile-text-line">quinncolello1 at gmail dot com</span>
+                    <span className="profile-text-line">qcolello@berkeley.edu</span>
                     <span className="profile-text-line">
-                        <a href="https://www.linkedin.com/in/quinncolello/" target="_blank">LinkedIn</a> - <a href="https://github.com/joebangles" target="_blank">GitHub</a>
+                        <a href="https://github.com/joebangles" target="_blank">
+                            <img className="inline-logo" src={github_logo}/> github
+                        </a>
+                    </span>
+                    <span className="profile-text-line">
+                        <a href="https://www.linkedin.com/in/quinncolello/" target="_blank">
+                            <img className="inline-logo" src={linkedin_logo}/> linkedin
+                        </a>
                     </span>
                 </div>
             </div>
-            <div className="frontpage-right-column">
-                <div className="profile">
-                    Undergraduate studying Computer Science and Applied Mathematics at UC Berkeley. I'm graduating in 2027.
+            <div className="frontpage-main-content">
+                <div>
+                    Third year studying Computer Science and Applied Mathematics at <a href="https://www.berkeley.edu" target="_blank">UC Berkeley</a>.
                     <br />
                     <br />
-                    I want to build real code powered by interesting theory. 
+                    Building real code powered by interesting theory. 
                 </div>
             </div>
-        </div>
-        </>
+        </div>  
     )
 }
 
