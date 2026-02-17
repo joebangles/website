38ec94ee9b11ec455361d5af069501c111185007
Joebangles <qhcbest@gmail.com>
2026-02-17 00:16:56 -0800

    Changed resume and generalized for future changes

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -10,7 +10,7 @@ function SiteHeader() {
                         <Link to="/" className="navlink navlink--type-home">Home</Link>
                         <Link to="/learning" className="navlink navlink--position-leftmost">Learning</Link>
                         {/* <Link to="/other" className="navlink">Other</Link> */}
-                        <a href="/colello_resume_feb26.pdf" target="_blank" rel="noopener noreferrer" className="navlink">CV</a>
+                        <a href="/colello_cv.pdf" target="_blank" rel="noopener noreferrer" className="navlink">CV↗</a>
                     </div>
                 </div>
             </header>
