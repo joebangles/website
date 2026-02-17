9e6c3e21c068470221cade44b62ee9578dfc1771
Joebangles <qhcbest@gmail.com>
2026-02-17 01:29:16 -0800

    Actually implemented site footer joke

--- src/components/SiteFooter/SiteFooter.tsx
+++ src/components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ function SiteFooter() {
     return <>
         <footer>
             <div className="subwidth-wrapper">
-                <p className="footer-text">© 2026 Quinn Colello. Powered by me.</p>
+                <p className="footer-text">© 2026 Quinn Colello. <a href='https://github.com/joebangles/website'>Powered by me.</a></p>
             </div>
         </footer>
     </>
